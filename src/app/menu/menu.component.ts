f6e7b96e4b2f8ff4d8d4d937599f9707f501bd0f
Matheus Wallace <faconntheus1@gmail.com>
2025-04-03 15:55:14 -0300

    menu do topo

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { MenuService } from '../services/menu.service';
+import { Menu } from '../models/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
 })
 export class MenuComponent {
+  constructor(private menuService: MenuService) {}
 
-  constructor(private menuService: MenuService) { }
+  menuStatus = 'closed';
+
+  menuItems?: Array<Menu>;
+
+  ngOnInit() {
+    const menu = this.menuService.getItems();
+
+    this.menuItems = menu;
+  }
+
+  menuClick(): void {
+    this.menuStatus = this.menuStatus == 'open' ? 'closed' : 'open';
+  }
 }
